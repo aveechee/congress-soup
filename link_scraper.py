b3d7892c4e403385c3f1dfb8c4e4c2cb829ac0db
aveechee <50119286+aveechee@users.noreply.github.com>
2022-04-06 10:28:02 -0700

    Add files via upload
    scrapy spider for getting the links for each session of the house of reps into a .csv file

---
+++ link_scraper.py
@@ -0,0 +1,18 @@
+#spider for webscraping the House of Representative session links 
+#from congress.gov into a .csv file
+
+
+import scrapy
+
+
+class LinkScraperSpider(scrapy.Spider):
+    name = 'link_scraper'
+    
+    custom_settings={"FEEDS": {"session_links.csv": {"format": "csv"}}}
+    
+    allowed_domains = ['https://www.congress.gov/roll-call-votes']
+    start_urls = ['https://www.congress.gov/roll-call-votes/']
+
+    def parse(self, response):
+        for link in response.xpath('//*[@id="content"]/div/div[2]/div[1]/ul/li/ul/li/a/@href').getall():
+            yield {'Link':link}
